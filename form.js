1c8445ad12b6c049ae6422f5008baaee3db22754
Duduardo2a <eduardoaraujof1@gmail.com>
2024-02-07 15:59:16 -0300

    atualizaçao feita no projeto, adicionando estilização e algumas melhorias no codigo

---
+++ form.js
@@ -0,0 +1,20 @@
+var alunos = [
+    {"nome":"Alanda", "matricula": 1234},
+    {"nome":"Luan", "matricula": 2345},
+    {"nome":"Caio", "matricula": 3456},
+    {"nome":"Wilson", "matricula": 4567},
+    {"nome":"Vitoria", "matricula": 5678},
+    {"nome":"Eduardo", "matricula": 5679},
+    {"nome":"Vitor", "matricula": 5688},
+    {"nome":"Camila", "matricula": 5578},
+    {"nome":"Joao", "matricula": 5378},
+    {"nome":"Maria", "matricula": 5688}
+];
+
+
+for(let i = 0; i < alunos.length; i++) {
+    document.write(alunos[i].nome);
+    }
+als = ["eduardo", "simples", "Quarta", "5"];
+als.sort();
+document.write(als);
